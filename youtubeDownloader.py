4c95fde88efae6966562b2c26885faefeba225b9
Simon0568 <simonkaradjordjevic12@gmail.com>
2022-09-15 22:06:45 +0200

    edit

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -8,4 +8,5 @@ w = yout.streams.get_highest_resolution()
 
 w.download(output_path="C:/Users/simon/OneDrive/Desktop/Python Projects") #the backslashes have to be facing this way, if you put "\" it will just be a normal string but that won't work since for this raw strings are required
 print("Download complete..")
+#wow
 
