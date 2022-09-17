56f8d674156eee73c18cfe6bd4ff59cabdda7fca
Simon0568 <simonkaradjordjevic12@gmail.com>
2022-09-18 00:32:25 +0200

    Implemented a better highscore system

--- numGuess.py
+++ numGuess.py
@@ -7,7 +7,14 @@ from time import sleep
 # Ako je na kraju broj poena veci(ili jednak) od proslog High Score, prosli highscore = broj poena i ispisuje se datum i vreme u kojem ja napravljen novi highscore, highscore se moze prikazati pritiskom na taster 2 iz glavnog menija
 # Ideja - postoji text file iz koga ce se citati highscore i tako omoguciti da se highscore sacuva i nakon gasenja programa.
 
-high_score = 0
+try:
+    file = open('high_score.txt', 'r')
+    high_score = file.read()
+    high_score = int(high_score[0])
+    file.close()
+except:
+    high_score = 0
+
 def menu():
     os.system('cls')
     print("Enter your choice:")
@@ -54,9 +61,10 @@ def game_start():
 
     random_num = random.randint(1, 10)
     print("This is a number guessing game, each time you play you'll be guessing a random number that i generated (1-10).\nAfter each miss, your points will be deducted, and if you reach zero... i win.")
-    sleep(7)
+    sleep(1) # PUT BACK TO 7
     while points > 0 and again == "yes":
         os.system('cls')
+        print(random_num) # REMOVE
         guess = int(input("Input your guess: "))
         if(guess != random_num):
             print("Mistake.")
@@ -78,18 +86,28 @@ def game_start():
             points = 5
             while again not in ["yes", "no"]:
                 again = input("Invalid choice, Do you want to play again? yes/no...\n").lower()
+    os.system('cls')
     print("Farewell...")
     high_score = num
+    high_score = str(high_score)
+    sleep(1)
+    file = open('high_score.txt', 'w')
+    file.write(high_score + f" - {high_name}")
+    file.close()
     menu()
 
 def high_scores():
+    file = open('high_score.txt', 'r')
+    high_score = file.read()
+    file.close()
     os.system('cls')
     try:
-        print(f"Current highscore: \n\n{high_score} - {high_name}")
+        print(f"Current highscore: \n\n{high_score}")
     except:
         print("There is no new high scores..")
     sleep(3)
     menu()
+    
 
             
 
