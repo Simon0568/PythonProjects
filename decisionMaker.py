2dccdd71c08c60cc42f8cfb4cb416b851cc37952
Simon0568 <simonkaradjordjevic12@gmail.com>
2022-09-19 00:45:21 +0200

    Simple Decision Maker

--- decisionMaker.py
+++ decisionMaker.py
@@ -2,20 +2,17 @@ import os
 import random
 
 
-def decisionMaker():
-    os.system('cls')
-    decisions = []
+os.system('cls')
+decisions = []
     
-    try:
-        n = int(input("How many decisions do you have?\n"))
-    except:
-        print("Not a number")
-        exit()
+try:
+     n = int(input("How many decisions do you have?\n"))
+except:
+    print("Not a number")
+    exit()
 
-    for i in range(n):
-        decision = input(f"Decision no {i+1}: ")
-        decisions.append(decision)
+for i in range(n):
+    decision = input(f"Decision no {i+1}: ")
+    decisions.append(decision)
 
-    print(f"You should try: {random.choice(decisions)}")
-
-decisionMaker()
+print(f"You should try: {random.choice(decisions)}")
